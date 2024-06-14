e90583536435a2e3204a99a95de59171cc4a219a
Ordin Sergei <osergey71@gmail.com>
2024-06-14 21:46:33 +0500

    fix

--- src/bot/models/user_model.py
+++ src/bot/models/user_model.py
@@ -10,13 +10,19 @@ class UserModel:
     @staticmethod
     async def get_user(user_id: int) -> User:
         """ Get user. """
-        return await User.get_or_none(user_id=user_id)
+        try:
+            return await User.get(user_id=user_id)
+        except Exception as e:
+            raise e
 
     @staticmethod
     async def create_user(user_id: int, user_name: str) -> None:
         """ Create user. """
-        user, created = await User.get_or_create(user_id=user_id, user_name=user_name)
-        if not created:
-            user.user_id = user_id
-            user.user_name = user_name
-            await user.save()
+        try:
+            user, created = await User.get_or_create(user_id=user_id, user_name=user_name)
+            if not created:
+                user.user_id = user_id
+                user.user_name = user_name
+                await user.save()
+        except Exception as e:
+            raise e
